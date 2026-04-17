50a3ea0cf1146e084a30fd0821049c7675e26a31
econ-dev-avishka <avishka@econsulate.net>
2026-04-17 18:49:09 +0530

    feat: add illumination correction using homomorphic filtering with explanatory comments

---
+++ q12.py
@@ -0,0 +1,18 @@
+"""12)  Illumination Correction using Homomorphic Filtering4:
+An image affected by non-uniform illumination can be modeled as:
+f (x, y) = i(x, y)·r(x, y),
+where i(x,y) represents illumination and r(x,y) represents reflectance.
+(a) Explain the significance of this multiplicative model.
+(b) Describe how a logarithmic transformation can be used to separate illumination and
+reflectance.
+(c) Propose an algorithm (or pseudo-code) for homomorphic filtering, including:
+• Logtransformation
+• Frequency-domain filtering
+• Inverse transformation
+(d) Compare homomorphic filtering with histogram equalization. In what situations would
+homomorphic filtering be preferred?
+(e) Apply the method to an image of your choice and briefly comment on:
+• Illumination correction
+• Contrast enhancement
+• Anyartifacts observed
+"""
